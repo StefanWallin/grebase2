24e3f9b7caef120819a1d1df7745f07bd793f3ac
Stefan Wallin <github@stefan-wallin.se>
2022-09-16 12:15:16 +0200

    Correct spelling of poem

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-console.log("Violets are red,")
-console.log("Roses are blue,")
+console.log("Roses are red,")
+console.log("Violets are blue,")
 console.log("Sugar is sweet,");
 console.log("And so are you.");
